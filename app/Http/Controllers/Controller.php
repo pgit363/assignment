b8516b1e38682169a8cd396e59b6ee222ce48dd9
pranav32 <pranav@indigitalit.com>
2022-06-11 21:13:07 +0530

    basic Auth

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+    * success response method.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function sendResponse($result, $message)
+   {
+       $response = [
+           'success' => true,
+           'message' => $message,
+           'data'    => $result,
+       ];
+
+       return response()->json($response, 200);
+   }
+
+
+   /**
+    * return error response.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function sendError($error, $errorMessages = [], $code )
+   {
+       $response = [
+           'success' => false,
+           'message' => $error,
+       ];
+
+       if(!empty($errorMessages)){
+           $response['data'] = $errorMessages;
+       }
+
+       return response()->json($response, $code);
+   }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\UserAuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/register', [UserAuthController::class, 'register']);
+Route::post('/login', [UserAuthController::class, 'login']);
+
+
+Route::group(['middleware' => 'api'], function ($router) {
+    Route::get('/student', [LandingPageController::class, 'index']);   
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
