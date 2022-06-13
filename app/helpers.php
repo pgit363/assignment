7b9a0b0852e4e72a1bd8e3fcb63048d085254a20
pranav32 <pranav@indigitalit.com>
2022-06-13 10:38:10 +0530

    helper

--- app/helpers.php
+++ app/helpers.php
@@ -2,7 +2,7 @@
 
 function countryByCode($code)
 {
-    Log::info("geting country by given code");
+    // Log::info("geting country by given code");
     $client = new GuzzleHttp\Client();
 
     $res = $client->request('GET', 'http://api.countrylayer.com/v2/callingcode/'.$code.'?access_key=700ac46828d90b7e9e7d7265e333c689', [
@@ -10,8 +10,8 @@ function countryByCode($code)
     ]);
 
     $res->getStatusCode(); 
-    Log::info(json_decode($res->getBody()->getContents(), true));
-    Log::info("country api successful");
+    // Log::info(json_decode($res->getBody()->getContents(), true));
+    // Log::info("country");
     return json_decode($res->getBody()->getContents(), true);
 }
 ?>
